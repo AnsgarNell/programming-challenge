489f90206c52302f231fe3b64a1b8ed222794f8d
Ansgar Nell <ansgar.nell@gmail.com>
2019-07-10 00:28:04 +0200

    Refactored WeatherDataParserTest deleting unnecessary tests.
    Fixed testGetEmptyParsedData to get really empty and not wrong data.
    Deleted testLoadEmptyData, testLoadData and testParseEmptyData as they are almost the same as other tests.

--- src/test/java/de/exxcellent/challenge/data/parsers/WeatherDataParserTest.java
+++ src/test/java/de/exxcellent/challenge/data/parsers/WeatherDataParserTest.java
@@ -21,38 +21,11 @@ class WeatherDataParserTest {
 	}
 	
 	@Test
-	void testLoadEmptyData() {
-		WeatherDataParser instance = new WeatherDataParser();
-		List<List<String>> data = new ArrayList<>();	
-		instance.loadData(data);
-	}
-	
-	@Test
-	void testLoadData() {
-		List<List<String>> data = Arrays.asList(
-				Arrays.asList("Day","MxT","MnT","AvT","AvDP","1HrP TPcpn","PDir","AvSp","Dir","MxS","SkyC","MxR","Mn","R AvSLP"),
-				Arrays.asList("1","88","59","74","53.8","0","280","9.6","270","17","1.6","93","23","1004.5"),
-				Arrays.asList("2","79","63","71","46.5","0","330","8.7","340","23","3.3","70","28","1004.5"),
-				Arrays.asList("3","77","55","66","39.6","0","350","5","350","9","2.8","59","24","1016.8")
-		);
-		WeatherDataParser instance = new WeatherDataParser();	
-		instance.loadData(data);
-	}
-	
-	@Test
 	void testParseNullData() {
 		WeatherDataParser instance = new WeatherDataParser();
 		Assertions.assertThrows(NullPointerException.class, () -> {
 			instance.parseRawData();
 		  });
-	}
-	
-	@Test
-	void testParseEmptyData() {
-		WeatherDataParser instance = new WeatherDataParser();
-		List<List<String>> data = new ArrayList<>();	
-		instance.loadData(data);
-		instance.parseRawData();
 	}
 	
 	@Test
@@ -134,12 +107,7 @@ class WeatherDataParserTest {
 	@Test
 	void testGetEmptyParsedData() {
 		WeatherDataParser instance = new WeatherDataParser();
-		List<List<String>> data = Arrays.asList(
-				Arrays.asList("Team","Games","Wins","Losses","Draws","Goals","Goals Allowed","Points"),
-				Arrays.asList("Arsenal","38","26","9","3","79","36","87"),
-				Arrays.asList("Liverpool","38","24","8","6","67","30","80"),
-				Arrays.asList("Manchester United","38","24","5","9","87","45","77")
-		);	
+		List<List<String>> data = new ArrayList<>();	
 		instance.loadData(data);
 		instance.parseRawData();
 		List<Object> result = instance.getParsedData();
