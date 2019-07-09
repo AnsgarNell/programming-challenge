5cdb984f89ffa19d85736e97976c4a6cb0f081ef
Ansgar Nell <ansgar.nell@gmail.com>
2019-07-09 18:58:56 +0200

    Added closeSource method to JSON and Web Service reading classes.

--- src/main/java/de/exxcellent/challenge/data/readers/web/services/WebServiceDataReader.java
+++ src/main/java/de/exxcellent/challenge/data/readers/web/services/WebServiceDataReader.java
@@ -32,5 +32,10 @@ public class WebServiceDataReader extends ChallengeDataReader{
     public List<List<String>> getData() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+	@Override
+	public void closeSource() throws Exception {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.	
+	}
     
 }
