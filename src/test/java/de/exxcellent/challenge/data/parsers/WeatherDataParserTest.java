f62929dd9d902e88fc4b834bbeb8721f42b80ef6
Ansgar Nell <ansgar.nell@gmail.com>
2019-07-09 21:27:49 +0200

    Update WeatherDataParserTest.java
    Added tests for parseRawData.

--- src/test/java/de/exxcellent/challenge/data/parsers/WeatherDataParserTest.java
+++ src/test/java/de/exxcellent/challenge/data/parsers/WeatherDataParserTest.java
@@ -56,21 +56,6 @@ class WeatherDataParserTest {
 	}
 	
 	@Test
-	void testParseData() {
-		List<List<String>> data = Arrays.asList(
-				Arrays.asList("Team","Games","Wins","Losses","Draws","Goals","Goals Allowed","Points"),
-				Arrays.asList("Arsenal","38","26","9","3","79","36","87"),
-				Arrays.asList("Liverpool","38","24","8","6","67","30","80"),
-				Arrays.asList("Manchester United","38","24","5","9","87","45","77")
-		);
-		WeatherDataParser instance = new WeatherDataParser();	
-		instance.loadData(data);
-		instance.parseRawData();
-		assertFalse("The parsed data can not be empty", instance.parsedData.isEmpty());
-	}
-	
-	/**
-	@Test
 	void testParseWrongData() {
 		List<List<String>> data = Arrays.asList(
 				Arrays.asList("Team","Games","Wins","Losses","Draws","Goals","Goals Allowed","Points"),
@@ -81,6 +66,62 @@ class WeatherDataParserTest {
 		WeatherDataParser instance = new WeatherDataParser();	
 		instance.loadData(data);
 		instance.parseRawData();
+		assertTrue("The parsed data must be empty", instance.parsedData.isEmpty());
+		
+		data = Arrays.asList(
+				Arrays.asList("Day","Games","Wins","Losses","Draws","Goals","Goals Allowed","Points"),
+				Arrays.asList("Arsenal","38","26","9","3","79","36","87"),
+				Arrays.asList("Liverpool","38","24","8","6","67","30","80"),
+				Arrays.asList("Manchester United","38","24","5","9","87","45","77")
+		);	
+		instance.loadData(data);
+		instance.parseRawData();
+		assertTrue("The parsed data must be empty", instance.parsedData.isEmpty());
+		
+		data = Arrays.asList(
+				Arrays.asList("Day","MxT","Wins","Losses","Draws","Goals","Goals Allowed","Points"),
+				Arrays.asList("Arsenal","38","26","9","3","79","36","87"),
+				Arrays.asList("Liverpool","38","24","8","6","67","30","80"),
+				Arrays.asList("Manchester United","38","24","5","9","87","45","77")
+		);	
+		instance.loadData(data);
+		instance.parseRawData();
+		assertTrue("The parsed data must be empty", instance.parsedData.isEmpty());
+		
+		data = Arrays.asList(
+				Arrays.asList("Day","MxT","MnT","Losses","Draws","Goals","Goals Allowed","Points"),
+				Arrays.asList("Arsenal","38","26","9","3","79","36","87"),
+				Arrays.asList("Liverpool","38","24","8","6","67","30","80"),
+				Arrays.asList("Manchester United","38","24","5","9","87","45","77")
+		);	
+		instance.loadData(data);
+		instance.parseRawData();
+		assertTrue("The parsed data must be empty", instance.parsedData.isEmpty());
+		
+		data = Arrays.asList(
+				Arrays.asList("Day","MxT","MnT","Losses","Draws","Goals","Goals Allowed","Points"),
+				Arrays.asList("1","88","59","9","3","79","36","87"),
+				Arrays.asList("Liverpool","38","24","8","6","67","30","80"),
+				Arrays.asList("Manchester United","38","24","5","9","87","45","77")
+		);	
+		instance.loadData(data);
+		instance.parseRawData();
+		assertTrue("The parsed data must contain only one parsed object", instance.parsedData.size() == 1);
 	}
-	*/
+	
+	@Test
+	void testParseData() {
+		List<List<String>> data = Arrays.asList(
+				Arrays.asList("Day","MxT","MnT","AvT","AvDP","1HrP TPcpn","PDir","AvSp","Dir","MxS","SkyC","MxR","Mn","R AvSLP"),
+				Arrays.asList("1","88","59","74","53.8","0","280","9.6","270","17","1.6","93","23","1004.5"),
+				Arrays.asList("2","79","63","71","46.5","0","330","8.7","340","23","3.3","70","28","1004.5"),
+				Arrays.asList("3","77","55","66","39.6","0","350","5","350","9","2.8","59","24","1016.8")
+		);
+		WeatherDataParser instance = new WeatherDataParser();	
+		instance.loadData(data);
+		instance.parseRawData();
+		assertFalse("The parsed data can not be empty", instance.parsedData.isEmpty());
+		assertTrue("The parsed data must contain three parsed objects", instance.parsedData.size() == 3);
+	}
+	
 }
