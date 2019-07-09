31a61516863f50f9bc29bec3e6b9c9dfb26f2275
Ansgar Nell <ansgar.nell@gmail.com>
2019-07-10 00:07:36 +0200

    Added temperature spread to WeatherData.
    Added member, getter and calculator for the temperature spread.

--- src/main/java/de/exxcellent/challenge/data/WeatherData.java
+++ src/main/java/de/exxcellent/challenge/data/WeatherData.java
@@ -5,8 +5,12 @@ public final class WeatherData {
 	private int day;
 	private double mxT;
 	private double mnT;
+	private double tSp = 0.0;
 	
 	public WeatherData() {
+		this.day = 0;
+		this.mxT = 0.0;
+		this.mnT = 0.0;
 	}
 	
 	public WeatherData(int day, double mxT, double mnT) {
@@ -32,6 +36,14 @@ public final class WeatherData {
 	}
 	public void setMnT(double mnT) {
 		this.mnT = mnT;
+	}
+	
+	public double getTsp() {
+		return tSp;
+	}
+	
+	public void calculateTemperatureSpread() {
+		this.tSp = this.mxT - this.mnT;
 	}
 
 }
