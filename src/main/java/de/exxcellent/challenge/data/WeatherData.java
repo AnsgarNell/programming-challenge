c2f5418574a8bf693cc361af61e259e7c9f3af59
Ansgar Nell <ansgar.nell@gmail.com>
2019-07-09 21:15:35 +0200

    Create WeatherData.java
    Data model to store weather data.

---
+++ src/main/java/de/exxcellent/challenge/data/WeatherData.java
@@ -0,0 +1,28 @@
+package de.exxcellent.challenge.data;
+
+public final class WeatherData {
+	
+	private int Day;
+	private double MxT;
+	private double MnT;
+	
+	public int getDay() {
+		return Day;
+	}
+	public void setDay(int day) {
+		Day = day;
+	}
+	public double getMxT() {
+		return MxT;
+	}
+	public void setMxT(double mxT) {
+		MxT = mxT;
+	}
+	public double getMnT() {
+		return MnT;
+	}
+	public void setMnT(double mnT) {
+		MnT = mnT;
+	}
+
+}
