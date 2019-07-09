471d19a44729ff99f698063d26399b524a0ae593
Ansgar Nell <ansgar.nell@gmail.com>
2019-07-09 19:02:21 +0200

    Update CSVFileReaderTest.java
    Added const for resources folder and weather and empty CSV files.
    Included assertion and source closing for testOpenSource.
    Implemented tests for closeSource, readData and getData methods.

--- src/test/java/de/exxcellent/challenge/data/readers/files/CSVFileReaderTest.java
+++ src/test/java/de/exxcellent/challenge/data/readers/files/CSVFileReaderTest.java
@@ -1,13 +1,19 @@
 package de.exxcellent.challenge.data.readers.files;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CSVFileReaderTest {
+	
+	private static final String RESOURCES_FOLDER = "src/main/resources/de/exxcellent/challenge/";
+	private static final String WEATHER_FILE = RESOURCES_FOLDER + "weather.csv";
+	private static final String EMPTY_FILE = RESOURCES_FOLDER + "empty.csv";
 
 	@Test
 	void testOpenEmptySource() {
@@ -18,16 +24,70 @@ class CSVFileReaderTest {
 	}
 	
 	@Test
-	void testOpenSource() throws FileNotFoundException {
-		CSVFileReader instance = new CSVFileReader("src/main/resources/de/exxcellent/challenge/weather.csv");
+	void testOpenSource() throws NullPointerException, IOException {
+		CSVFileReader instance = new CSVFileReader(WEATHER_FILE);
 		instance.openSource();
+		assertFalse("BufferedReader must not be null", instance.br == null);
+		// If we open the file, we have to close it to be able to continue testing
+		instance.closeSource();
 	}
 	
 	@Test
-	void testReadData() throws FileNotFoundException {
-		CSVFileReader instance = new CSVFileReader("src/main/resources/de/exxcellent/challenge/weather.csv");
+	void testCloseEmptySource() throws Exception {
+		CSVFileReader instance = new CSVFileReader("");
+		Assertions.assertThrows(NullPointerException.class, () -> {
+		    instance.closeSource();
+		  });
+	}
+	
+	@Test
+	void testCloseFile() throws NullPointerException, IOException  {
+		CSVFileReader instance = new CSVFileReader(WEATHER_FILE);
+		instance.openSource();
+		instance.closeSource();
+		// TODO: Must we or can we check this? We just call BufferedReader close() method.
+	}
+	
+	@Test
+	void testReadDataWithoutOpeningTheFile() throws IOException {
+		CSVFileReader instance = new CSVFileReader(WEATHER_FILE);
+		Assertions.assertThrows(NullPointerException.class, () -> {
+		    instance.readData();
+		  });
+	}
+	
+	@Test
+	void testReadData() throws IOException {
+		CSVFileReader instance = new CSVFileReader(WEATHER_FILE);
 		instance.openSource();
 		instance.readData();
+		instance.closeSource();
+	}
+	
+	@Test
+	void testGetDataWithoutOpeningTheFile() throws IOException {
+		CSVFileReader instance = new CSVFileReader(WEATHER_FILE);
+		List<List<String>> result = instance.getData();
+		assertTrue("getData() must return an empty list", result.isEmpty());
+	}
+	
+	@Test
+	void testGetDataWithEmptyFile() throws IOException {
+		CSVFileReader instance = new CSVFileReader(EMPTY_FILE);
+		instance.openSource();
+		instance.readData();
+		List<List<String>> result = instance.getData();
+		instance.closeSource();
+		assertTrue("getData() must return an empty list", result.isEmpty());
 	}
 
+	@Test
+	void testGetData() throws IOException {
+		CSVFileReader instance = new CSVFileReader(WEATHER_FILE);
+		instance.openSource();
+		instance.readData();
+		List<List<String>> result = instance.getData();
+		instance.closeSource();
+		assertFalse("getData() must return a list", result.isEmpty());
+	}
 }
