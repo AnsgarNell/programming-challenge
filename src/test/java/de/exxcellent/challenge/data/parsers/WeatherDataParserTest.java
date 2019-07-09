a1cfc5b1dda0d0842b02ad17285abe5599719e0d
Ansgar Nell <ansgar.nell@gmail.com>
2019-07-09 21:39:01 +0200

    Update WeatherDataParserTest.java
    Added tests for parseRawData method.

--- src/test/java/de/exxcellent/challenge/data/parsers/WeatherDataParserTest.java
+++ src/test/java/de/exxcellent/challenge/data/parsers/WeatherDataParserTest.java
@@ -124,4 +124,41 @@ class WeatherDataParserTest {
 		assertTrue("The parsed data must contain three parsed objects", instance.parsedData.size() == 3);
 	}
 	
+	@Test
+	void testGetNullParsedData() {
+		WeatherDataParser instance = new WeatherDataParser();
+		List<Object> result = instance.getParsedData();
+		assertTrue("The result data must be null", result == null);
+	}
+	
+	@Test
+	void testGetEmptyParsedData() {
+		WeatherDataParser instance = new WeatherDataParser();
+		List<List<String>> data = Arrays.asList(
+				Arrays.asList("Team","Games","Wins","Losses","Draws","Goals","Goals Allowed","Points"),
+				Arrays.asList("Arsenal","38","26","9","3","79","36","87"),
+				Arrays.asList("Liverpool","38","24","8","6","67","30","80"),
+				Arrays.asList("Manchester United","38","24","5","9","87","45","77")
+		);	
+		instance.loadData(data);
+		instance.parseRawData();
+		List<Object> result = instance.getParsedData();
+		assertTrue("The result data must be empty", result.isEmpty());
+	}
+	
+	@Test
+	void testGetParsedData() {
+		List<List<String>> data = Arrays.asList(
+				Arrays.asList("Day","MxT","MnT","AvT","AvDP","1HrP TPcpn","PDir","AvSp","Dir","MxS","SkyC","MxR","Mn","R AvSLP"),
+				Arrays.asList("1","88","59","74","53.8","0","280","9.6","270","17","1.6","93","23","1004.5"),
+				Arrays.asList("2","79","63","71","46.5","0","330","8.7","340","23","3.3","70","28","1004.5"),
+				Arrays.asList("3","77","55","66","39.6","0","350","5","350","9","2.8","59","24","1016.8")
+		);
+		WeatherDataParser instance = new WeatherDataParser();	
+		instance.loadData(data);
+		instance.parseRawData();
+		List<Object> result = instance.getParsedData();
+		assertFalse("The result data can not be empty", result.isEmpty());
+	}
+	
 }
