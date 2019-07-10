e9edbafe3d201a2f21a846b74390d01acb13b7be
Ansgar Nell <ansgar.nell@gmail.com>
2019-07-10 21:36:18 +0200

    Added team variable to FootballData.

--- src/main/java/de/exxcellent/challenge/data/FootballData.java
+++ src/main/java/de/exxcellent/challenge/data/FootballData.java
@@ -2,16 +2,19 @@ package de.exxcellent.challenge.data;
 
 public final class FootballData {
 	
+	private String team;
 	private int goals;
 	private int goalsAllowed;
 	private int distance = 0;
 	
 	public FootballData() {
+		this.team = "";
 		this.goals = 0;
 		this.goals = 0;
 	}
 	
-	public FootballData(int goals, int goalsAllowed) {
+	public FootballData(String team, int goals, int goalsAllowed) {
+		this.team = team;
 		this.goals = goals;
 		this.goalsAllowed = goalsAllowed;
 	}
@@ -35,6 +38,14 @@ public final class FootballData {
 	
 	public void calculateDistance() {
 		this.distance = Math.abs(this.goals - this.goalsAllowed);
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
 	}
 
 }
